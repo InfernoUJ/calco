18a3104af4f9ea1642d4ad882967579e7178cb2c
InfernoUJ <mykhailo.petin@student.uj.edu.pl>
2024-03-15 17:25:37 +0100

    added data to chart

--- app/src/main/java/com/example/calco/ui/charts/pie/PieChartFragment.java
+++ app/src/main/java/com/example/calco/ui/charts/pie/PieChartFragment.java
@@ -1,5 +1,6 @@
 package com.example.calco.ui.charts.pie;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.calco.databinding.FragmentPieChartBinding;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.*;
 
 public class PieChartFragment extends Fragment {
     private FragmentPieChartBinding binding;
@@ -21,6 +29,23 @@ public class PieChartFragment extends Fragment {
 
         binding = FragmentPieChartBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        PieChart pieChart = binding.pieChart;
+        List<PieEntry> pieEntries = new ArrayList<>();
+        pieEntries.add(new PieEntry(18.5f, "Green"));
+        pieEntries.add(new PieEntry(26.7f, "Yellow"));
+        pieEntries.add(new PieEntry(24.0f, "Red"));
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Colors");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Colors");
+        pieChart.animate();
 
         return root;
     }
