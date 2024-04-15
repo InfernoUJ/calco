23b51fea37a9b8917810ebbffeab2d33f895e258
InfernoUJ <mykhailo.petin@student.uj.edu.pl>
2024-04-15 10:57:28 +0200

    percent convertor in dish logic refactored

--- app/src/main/java/com/example/calco/logic/business/DishLogic.java
+++ app/src/main/java/com/example/calco/logic/business/DishLogic.java
@@ -8,6 +8,7 @@ import com.example.calco.logic.persistent.entities.PDish;
 import com.example.calco.logic.persistent.entities.PHistoryOfProducts;
 import com.example.calco.logic.persistent.entities.PProduct;
 import com.example.calco.logic.persistent.entities.ProductsInDishes;
+import com.example.calco.logic.utils.PercentConvertor;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -51,50 +52,7 @@ public class DishLogic {
 
     // todo refactor
     private static List<Map.Entry<Product, Integer>> getProductsPercentImpact(List<Map.Entry<Product, Integer>> productsMass) {
-        int n = productsMass.size();
-        int totalMass = 0;
-        for(int i = 0; i < n; i++) {
-            totalMass += productsMass.get(i).getValue();
-        }
-        List<Integer> productsPercents = new ArrayList<>(n);
-        int currMassDifference = 0;
-        for (int i = 0; i < n; i++) {
-            int mass = productsMass.get(i).getValue();
-            double percent = (double) mass / totalMass * 100;
-            if (percent < 1) {
-                productsPercents.add(1);
-                currMassDifference += 1;
-            }
-            else {
-                if (currMassDifference >= 0) {
-                    productsPercents.add((int)Math.floor(percent));
-                    currMassDifference -= 1;
-                }
-                else {
-                    productsPercents.add((int)Math.ceil(percent));
-                    currMassDifference += 1;
-                }
-            }
-        }
-
-        // subtract extra weight from the heaviest
-        List<Integer> indices = IntStream.range(0, n)
-                .boxed()
-                .sorted((i1, i2) -> Integer.compare(productsMass.get(i2).getValue(), productsMass.get(i1).getValue()))
-                .limit(currMassDifference)
-                .collect(Collectors.toList());
-        for (int index: indices) {
-            productsPercents.set(index, productsPercents.get(index)-1);
-        }
-
-        List<Map.Entry<Product, Integer>> productsWithPercents = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (productsPercents.get(i) != 0) {
-                productsWithPercents.add(Map.entry(productsMass.get(i).getKey(), productsPercents.get(i)));
-            }
-        }
-
-        return productsWithPercents;
+        return PercentConvertor.getPercentImpact(productsMass);
     }
 
     private static void persistProductsInDish(long dishId, List<Map.Entry<Product, Integer>> productsPercentImpact) {
--- app/src/main/java/com/example/calco/logic/utils/PercentConvertor.java
+++ app/src/main/java/com/example/calco/logic/utils/PercentConvertor.java
@@ -2,11 +2,58 @@ package com.example.calco.logic.utils;
 
 import com.example.calco.logic.business.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PercentConvertor {
     public static <T> List<Map.Entry<T, Integer>> getPercentImpact(List<Map.Entry<T, Integer>> absoluteImpact) {
-        return null;
+        int n = absoluteImpact.size();
+        int totalMass = 0;
+        for(int i = 0; i < n; i++) {
+            totalMass += absoluteImpact.get(i).getValue();
+        }
+
+        List<Integer> entitiesPercents = new ArrayList<>(n);
+        int currMassDifference = 0;
+        for (int i = 0; i < n; i++) {
+            int mass = absoluteImpact.get(i).getValue();
+            double percent = (double) mass / totalMass * 100;
+            if (percent < 1) {
+                entitiesPercents.add(1);
+                currMassDifference += 1;
+            }
+            else {
+                if (currMassDifference >= 0) {
+                    entitiesPercents.add((int)Math.floor(percent));
+                    currMassDifference -= 1;
+                }
+                else {
+                    entitiesPercents.add((int)Math.ceil(percent));
+                    currMassDifference += 1;
+                }
+            }
+        }
+
+        // subtract extra weight from the heaviest
+        List<Integer> indices = IntStream.range(0, n)
+                .boxed()
+                .sorted((i1, i2) -> Integer.compare(absoluteImpact.get(i2).getValue(), absoluteImpact.get(i1).getValue()))
+                .limit(currMassDifference)
+                .collect(Collectors.toList());
+        for (int index: indices) {
+            entitiesPercents.set(index, entitiesPercents.get(index)-1);
+        }
+
+        List<Map.Entry<T, Integer>> entitiesWithPercents = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (entitiesPercents.get(i) != 0) {
+                entitiesWithPercents.add(Map.entry(absoluteImpact.get(i).getKey(), entitiesPercents.get(i)));
+            }
+        }
+
+        return entitiesWithPercents;
     }
 }
